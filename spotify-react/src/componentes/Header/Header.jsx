679935d49cb5222196e2674ec4c7a937482b6133
henriqueCostaSI <henrique.mcosta@hotmail.com>
2025-02-04 00:01:22 -0300

    Final

--- spotify-react/src/componentes/Header/Header.jsx
+++ spotify-react/src/componentes/Header/Header.jsx
@@ -7,57 +7,6 @@ import search from '../../assets/icons/search.png';
 
 
 function Header() {
-
-   
-    const resultArtist = document.getElementById("result-artist");
-    const resultPlaylists = document.getElementById('result-playlists');
-
-    const [busca, setBusca] = useState('');
-
-    async function requestApi(busca) {
-        if (busca === '') return;
-        
-        const url = `http://localhost:3001/artists?nameLike=${busca}`;
-        try {
-            const response = await fetch(url);
-            const result = await response.json();
-            displayResult(result);
-        } catch (error) {
-            console.error('Erro na busca:', error);
-        }
-    }
-
-    function displayResult(result) {
-        resultPlaylists.classList.add("hidden")
-        const artistName = document.getElementById('artist-name');
-        const artistImage = document.getElementById('artist-img');
-        result.forEach(element => {
-            artistName.innerText = element.name;
-            artistImage.src = element.urlImg;
-        });
-        resultArtist.classList.remove('hidden');
-    }
-        
-    
-    
-
-    const handleSearch = (e) => {
-        const valor = e.target.value.toLowerCase();
-        console.log(valor);
-        
-        if ( busca === '') {
-            resultPlaylists.classList.add('hidden');
-            resultArtist.classList.remove('hidden');
-            return;
-    
-        }
-        setBusca(valor);
-        requestApi(valor);
-    }
-
-
-
-
     
     return (
         <nav className="header__navigation">
@@ -78,8 +27,7 @@ function Header() {
                     autoCapitalize="off"
                     spellCheck="false"
                     placeholder="O que você quer ouvir?" 
-                    value = {busca}
-                    onChange = {handleSearch}/>
+                    />
             </div>
             <div className="header__login">
                 <button className="subscribe">Inscreva-se</button>
